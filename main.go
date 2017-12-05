db471e31d376db49e6b5e405bf2fc347e3b4745d
mohammad-yazdani <yazdani.mohammad106@live.co.uk>
2017-12-05 10:43:23 -0500

    timer added

--- main.go
+++ main.go
@@ -2,14 +2,39 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"os"
+	"errors"
 )
+
+func getGreeting(hour int) (string, error)  {
+	var message string
+	if hour < 7 {
+		err := errors.New("Too early, we're closed!")
+		return message, err
+	} else if hour < 12 {
+		message = "Good Morning"
+	} else if hour < 18 {
+		message = "Good Afternoon"
+	} else {
+		message = "Good evening"
+	}
+	return message, nil
+}
 
 func main() {
 	args := os.Args
+	hourOfDay := time.Now().Hour()
+	greeting, err := getGreeting(hourOfDay)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	if len(args) > 1 {
 		fmt.Println(args[1])
 	} else {
 		fmt.Println("Hello, I am Gopher")
 	}
+	fmt.Println(greeting)
 }
